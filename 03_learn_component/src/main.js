9db4cceb89c01009cc3bed0ec3eb978bd8b535b4
hujianguo1 <2224818693@qq.com>
2022-01-12 17:18:55 +0800

    webpack

--- 03_learn_component/src/main.js
+++ 03_learn_component/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
-// import App from './01_组件拆分和嵌套/App.vue'
+import App from './01_组件拆分和嵌套/App.vue'
 // import App from './02_组件的css作用域/App.vue'
 // import App from './03_父组件向子组件传数据/App.vue'
 // import App from './04_子组件向父组件传递/App.vue'
-import App from "./05_商品页面切换/App.vue"
+// import App from "./05_商品页面切换/App.vue"
 
 createApp(App).mount('#app')
