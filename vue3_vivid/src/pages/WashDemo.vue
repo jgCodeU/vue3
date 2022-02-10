a02e82b23594afbd776616437dfcadbf8d669db6
hujianguo1 <2224818693@qq.com>
2022-02-10 13:32:10 +0800

    应用示例

--- vue3_vivid/src/pages/WashDemo.vue
+++ vue3_vivid/src/pages/WashDemo.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- ****************启动暂停切换*************** -->
     <div class="runningPause">
         <h1>启动暂停切换</h1>
         <!-- 硬切换 -->
@@ -16,8 +17,31 @@
         <!-- 无论进度条的实际状态如何，都从0开始上涨到实际状态处 -->
     </div>
 
+    <!-- ****************弹框提示*************** -->
     <div class="showTips">
         <h1>弹框提示</h1>
+        <button @click="showTips" class="btn">显示弹框</button>
+        <!-- 硬切换 -->
+        <!-- <div class="tipContent" v-show="ifShowTips">
+            <p>提示信息提示信息提示信息</p>
+            <div class="closeBtn" @click="closeBtn">关闭弹框</div>
+        </div> -->
+        <!-- 软切换 -->
+        <transition name="showTips">
+            <div class="tipContent" v-show="ifShowTips">
+                <p>提示信息提示信息提示信息</p>
+                <div class="closeBtn" @click="closeBtn">关闭弹框</div>
+            </div>
+        </transition>
+    </div>
+
+    <!-- ****************切换区域显示内容*************** -->
+    <div class="runningStatus">
+        <h1>切换区域显示内容</h1>
+        <div class="tabContent">
+            <button>tab-1</button>
+            <button>tab-2</button>
+        </div>
     </div>
 
     <div class="block"></div>
@@ -33,6 +57,16 @@
     }
     function pause() {
         isRunning.value = false
+    }
+
+
+    //弹框提示
+    let ifShowTips = ref(false)
+    function showTips() {
+        ifShowTips.value = (true)
+    }
+    function closeBtn() {
+        ifShowTips.value = false
     }
 </script>
 
@@ -88,11 +122,46 @@
     margin: 0 auto;
     margin-top: 20px;
     width: 600px;
-    height: 200px;
+    height: 210px;
     border: 2px solid black;
 }
 
+.tipContent {
+    width: 200px;
+    border-radius: 10px;
+    /* padding: 5px; */
+    padding-top: 1px;
+    margin: 0 auto;
+    margin-top: 5px;
+    background-color: skyblue;
+}
+.closeBtn {
+    width: 100px;
+    background-color: slategray;
+    display: inline-block;
+    margin-bottom:5px ;
+    border-radius: 5px;
+}
+.showTips-enter-from,
+.showTips-leave-to {
+    opacity: 0;
+}
+.showTips-enter-active,
+.showTips-leave-active {
+    transition: opacity .5s;
+}
 
+/* 切换区域显示内容 */
+.runningStatus {
+    position: relative;
+    margin: 0 auto;
+    margin-top: 20px;
+    width: 600px;
+    height: 210px;
+    border: 2px solid black;
+}
+
+/* 下方留白 */
 .block {
     height: 200px;
 }
